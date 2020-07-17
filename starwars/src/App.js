a0dc8e57f6111cebc15b39d49227c4e9430708b9
kate roy <kroy15chs@gmail.com>
2020-07-17 12:58:24 -0400

    Need styling, but content is up and running smoothly

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect,} from 'react';
 import './App.css';
+import { BASE_URL } from './constants'
+import axios from 'axios'
+import Character from './components/Character'
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
+  const [characters, setCharacters] = useState([])
+
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  useEffect(() => {
+    axios.get(`${BASE_URL}api/people/`)
+    .then(response => {
+      console.log('happy path :)')
+      setCharacters(response.data.results)
+    })
+    .catch(err => {
+      console.log('sad path :(', err)
+    })
+  },[])
+
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {characters.map(character => {
+        return (<Character person={character}/>)
+      })}
     </div>
   );
 }
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,18 @@
 // Write your Character component here
+import React from 'react'
+
+const Character = (props) => {
+
+    const { person } = props
+    
+    return (
+        <div>
+            <h2>{person.name}</h2>
+            <h3>Birth Year:{person.birth_year}</h3>
+            <h3>Height: {person.height}</h3>
+            <a href={person.url}>{person.url}</a>
+        </div>
+    )
+}
+
+export default Character;
